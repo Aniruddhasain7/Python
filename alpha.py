33dfb60a880865037b336909b209b3571ae55a3b
Aniruddha Sain <aniruddhasain315@gmail.com>
2025-07-12 11:42:47 +0530

    Delete alpha.py

--- alpha.py
+++
@@ -1,8 +0,0 @@
-count=0
-f=open(r"C:\Users\aniru\Documents\intro.txt",'r')
-rd=f.read()
-for i in range(len(rd)):
-    if rd[i].isupper():
-        count+=1
-print("Number of uppercase alphabet:",count)
-f.close()
