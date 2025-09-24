06d21f82f94647be257165beff838dfffca9ea7c
Aniruddha Sain <aniruddhasain315@gmail.com>
2025-09-24 19:01:13 +0530

    Delete linear_search.py

--- linear_search.py
+++
@@ -1,9 +0,0 @@
-L=[1,2,3,4,5,6,7]
-n=int(input("Enter item to be searched:"))
-a=len(L)
-for i in range(a):
-    if L[i]==n:
-        print("Item found")
-    else:
-        print("Item not found")
-        break
